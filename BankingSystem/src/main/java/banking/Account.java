c60aaeda2d1d5a281323b3bb845cf14c3ba1d4fc
aisha2093 <117574513+aisha2093@users.noreply.github.com>
2022-11-06 17:35:05 +0530

    Update Account.java

--- BankingSystem/src/main/java/banking/Account.java
+++ BankingSystem/src/main/java/banking/Account.java
@@ -17,7 +17,10 @@ public abstract class Account {
 	private double balance;
 
 	protected Account(AccountHolder accountHolder, Long accountNumber, int pin, double startingDeposit) {
-		// complete the constructor
+		this.accountHolder = accountHolder;
+		this.accountNumber = accountNumber;
+		this.pin = pin;
+		this.startingDeposit = startingDeposit;
 	}
 
 	public AccountHolder getAccountHolder() {
